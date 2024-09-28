1179650b234717052a728d17698de238451f9985
Augusto Polonio <augustodrp@gmail.com>
2024-09-28 20:31:03 -0300

    fix: the username feat was incomplete

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,15 +4,13 @@ import { useState } from "react";
 import { data } from "@/mocks/items";
 
 export default function Home() {
-	const [username, setUsername] = useState("");
-
 	return (
 		<main className="flex flex-col gap-2">
 			<div className="flex items-center gap-2">
 				<ChartLine />
 				<h1 className="text-3xl font-semibold">ResultTrack</h1>
 			</div>
-			<h1 className="text-xl font-semibold">Olá, {username}!</h1>
+			<h1 className="text-xl font-semibold">Olá, Joh Doe!</h1>
 
 			{data.map((d, i) => (
 				<InputCard key={d.id} {...d} />
