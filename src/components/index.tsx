e332dc2db58dffa5fac5190293b41fe50055d729
Augusto Polonio <augustodrp@gmail.com>
2024-09-28 20:26:42 -0300

    refactor: separate the mock data

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,41 +1,10 @@
 import { ChartLine } from "lucide-react";
 import InputCard from "./custom/input_card";
-import lavandaImg from "../assets/lavanda.png";
-import cravoImg from "../assets/cravo.png";
-import limaoImg from "../assets/limao.png";
-import eucaliptoImg from "../assets/eucalipto.png";
+import { useState } from "react";
+import { data } from "@/mocks/items";
 
 export default function Home() {
-	const data = [
-		{
-			id: 1,
-			title: "Lavanda",
-			description:
-				"Planta conhecida por suas propriedades tranquilizantes e aroma agradável.",
-			imageSrc: lavandaImg.src,
-		},
-		{
-			id: 2,
-			title: "Cravo",
-			description:
-				"Flor decorativa e aromática, também usada como tempero com propriedades medicinais.",
-			imageSrc: cravoImg.src,
-		},
-		{
-			id: 3,
-			title: "Limão",
-			description:
-				"Fruta cítrica rica em vitamina C, amplamente utilizada em culinária e bebidas.",
-			imageSrc: limaoImg.src,
-		},
-		{
-			id: 4,
-			title: "Eucalipto",
-			description:
-				"Árvore cujas folhas são usadas para fins medicinais e aromaterapia devido ao seu óleo essencial.",
-			imageSrc: eucaliptoImg.src,
-		},
-	];
+	const [username, setUsername] = useState("");
 
 	return (
 		<main className="flex flex-col gap-2">
@@ -43,7 +12,7 @@ export default function Home() {
 				<ChartLine />
 				<h1 className="text-3xl font-semibold">ResultTrack</h1>
 			</div>
-			<h1 className="text-xl font-semibold">Olá, John Doe!</h1>
+			<h1 className="text-xl font-semibold">Olá, {username}!</h1>
 
 			{data.map((d, i) => (
 				<InputCard key={d.id} {...d} />
---
+++ src/mocks/items.ts
@@ -0,0 +1,35 @@
+import lavandaImg from "../assets/lavanda.png";
+import cravoImg from "../assets/cravo.png";
+import limaoImg from "../assets/limao.png";
+import eucaliptoImg from "../assets/eucalipto.png";
+
+export const data = [
+	{
+		id: 1,
+		title: "Lavanda",
+		description:
+			"Planta conhecida por suas propriedades tranquilizantes e aroma agradável.",
+		imageSrc: lavandaImg.src,
+	},
+	{
+		id: 2,
+		title: "Cravo",
+		description:
+			"Flor decorativa e aromática, também usada como tempero com propriedades medicinais.",
+		imageSrc: cravoImg.src,
+	},
+	{
+		id: 3,
+		title: "Limão",
+		description:
+			"Fruta cítrica rica em vitamina C, amplamente utilizada em culinária e bebidas.",
+		imageSrc: limaoImg.src,
+	},
+	{
+		id: 4,
+		title: "Eucalipto",
+		description:
+			"Árvore cujas folhas são usadas para fins medicinais e aromaterapia devido ao seu óleo essencial.",
+		imageSrc: eucaliptoImg.src,
+	},
+];
