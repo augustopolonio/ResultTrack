a37d646bdd0ca097a3b975e7a7d45c03cd21a0b2
Augusto Polonio <augustodrp@gmail.com>
2024-10-02 20:30:06 -0300

    feat: created a tab menu; added a favicon to the project

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,15 @@
-import { Bird, ChartLine } from "lucide-react";
+import {
+	Bird,
+	ChartColumnDecreasingIcon,
+	ChartLine,
+	HomeIcon,
+} from "lucide-react";
 import InputCard from "./custom/input_card";
 import { useState } from "react";
 import { data } from "@/mocks/items";
 import { Button } from "./ui/button";
 import InputUsername from "./custom/input_username";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
 	const [username, setUsername] = useState(
@@ -23,16 +29,36 @@ export default function Home() {
 				<InputUsername setUsername={setUsername} />
 			)}
 
-			{data.length > 0 ? (
-				data.map((d, i) => <InputCard key={d.id} {...d} />)
-			) : (
-				<div className="h-[700px] flex flex-col items-center justify-center gap-5">
-					{/*"Add a lucide react icon to show the user that there is no data"*/}
-					<Bird size={120} className="animate-bounce duration-2000" />
-					Nothing to track
-					<Button>New tack</Button>
-				</div>
-			)}
+			<div>
+				<Tabs defaultValue="home">
+					<TabsContent value="home" className="flex flex-col gap-2">
+						{data.length > 0 ? (
+							data.map((d, i) => <InputCard key={d.id} {...d} />)
+						) : (
+							<div className="h-[700px] flex flex-col items-center justify-center gap-5">
+								{/*"Add a lucide react icon to show the user that there is no data"*/}
+								<Bird size={120} className="animate-bounce duration-2000" />
+								Nothing to track
+								<Button>New tack</Button>
+							</div>
+						)}
+					</TabsContent>
+					<TabsContent value="statistics">
+						Change your password here.
+					</TabsContent>
+
+					<TabsList className="flex justify-center h-15 fixed bottom-0 left-0 w-full">
+						<TabsTrigger value="home" className="flex flex-col gap-1">
+							<HomeIcon size={20} />
+							Home
+						</TabsTrigger>
+						<TabsTrigger value="statistics" className="flex flex-col gap-1">
+							<ChartColumnDecreasingIcon size={20} />
+							Statistics
+						</TabsTrigger>
+					</TabsList>
+				</Tabs>
+			</div>
 		</main>
 	);
 }
