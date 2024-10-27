74653668a1987026c298dab392a59582c73a3184
Augusto Polonio <augustodrp@gmail.com>
2024-10-27 18:22:36 -0300

    style: sticky the header and mantain the scroll in the center of screen

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,10 @@
-import { Bird, ChartColumnDecreasingIcon, HomeIcon } from "lucide-react";
+import {
+	Bird,
+	ChartColumnDecreasingIcon,
+	ChartPie,
+	HomeIcon,
+	List,
+} from "lucide-react";
 import InputCard from "./custom/input_card";
 import { useState } from "react";
 import { data } from "@/mocks/items";
@@ -24,22 +30,25 @@ export default function Home() {
 
 	return (
 		<>
-			<div className="h-5 animated-background bg-gradient-to-r from-pink-500 via-violet-500 to-green-500 animate-gradient-x" />
-			<main className="flex flex-col gap-2 p-4">
-				<div className="flex items-center gap-2">
-					<img src={icon.src} alt="" width={30} height={30} />
-					<h1 className="text-3xl font-semibold">Smell Retraining Therapy</h1>
+			<div className="h-5 animated-background bg-gradient-to-r from-pink-500 via-violet-500 to-green-500 animate-gradient-x sticky top-0" />
+			<main className="flex flex-col gap-2">
+				{/* <div className="sticky top-5 backdrop-blur-lg drop-shadow-md rounded-lg px-4 py-2"> */}
+				<div className="sticky top-5 bg-white border-b drop-shadow-md rounded-lg px-4 py-2">
+					<div className="flex items-center gap-2">
+						<img src={icon.src} alt="" width={30} height={30} />
+						<h1 className="text-2xl font-semibold">Smell Retraining Therapy</h1>
+					</div>
+
+					{username ? (
+						<h1 className="text-xl">
+							{getGreeting()}, {username.split(" ")[0]}!
+						</h1>
+					) : (
+						<InputUsername setUsername={setUsername} />
+					)}
 				</div>
 
-				{username ? (
-					<h1 className="text-xl">
-						{getGreeting()}, {username.split(" ")[0]}!
-					</h1>
-				) : (
-					<InputUsername setUsername={setUsername} />
-				)}
-
-				<div>
+				<div className="flex flex-col px-4 pb-20">
 					<Tabs defaultValue="home">
 						<TabsContent value="home" className="flex flex-col gap-2">
 							{data.length > 0 ? (
@@ -53,17 +62,21 @@ export default function Home() {
 								</div>
 							)}
 						</TabsContent>
-						<TabsContent value="statistics">
+						<TabsContent value="list">
 							<Statistics />
 						</TabsContent>
 
-						<TabsList className="flex justify-center h-15 fixed bottom-0 left-0 w-full">
+						<TabsList className="flex justify-evenly h-15 fixed bottom-0 left-0 w-full">
 							<TabsTrigger value="home" className="flex flex-col gap-1">
 								<HomeIcon size={20} />
 								Home
 							</TabsTrigger>
+							<TabsTrigger value="list" className="flex flex-col gap-1">
+								<List size={20} />
+								List
+							</TabsTrigger>
 							<TabsTrigger value="statistics" className="flex flex-col gap-1">
-								<ChartColumnDecreasingIcon size={20} />
+								<ChartPie size={20} />
 								Statistics
 							</TabsTrigger>
 						</TabsList>
